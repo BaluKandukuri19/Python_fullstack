5438ecc0e44b8e4caa14bc92ce3acc15ccb42f67
BaluKandukuri19 <balakandukuri30@gmail.com>
2026-08-05 10:15:56 +0000

    balu

---
+++ app.py
@@ -0,0 +1,62 @@
+
+from flask import Flask,render_template,jsonify,request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/courses')
+def courses():
+    return render_template("courses.html")
+
+@app.route('/trainers')
+def trainers():
+    return render_template("trainers.html")
+
+@app.route('/register',methods=["POST","GET"])
+def register():
+    if request.method=="POST":
+        name=request.form["name"]
+        email=request.form["email"]
+        password=request.form["password"]
+        dob=request.form["dob"]
+        gender=request.form["gender"]
+        course=request.form["course"]
+        return render_template("register.html")
+    return render_template("register.html")
+
+
+
+@app.route('/api/register',methods=["POST"])
+def api_register():
+    data=request.get_json()
+    email=data.get("email")
+    if email in users_db:
+        return jsonify({"status":"error","message":"user already exists thisemail"}),400
+        users_db[email]=data
+    else:
+        return jsonify({"status":"succes","message":"Registration Successful!"})
+
+@app.route('/api/login',methods=["POST"])
+def login():
+    data=request.get_json()
+    email=data.get("email")
+    password=data.get("password")
+    if email in users_db:
+        return jsonify({"status":"success","message":"Login successful!"})
+        
+    else:
+        return jsonify({"status":"error","message":"Invalid eamil or password!"}),401
+
+if __name__ == '__main__':
+    app.run(debug=True)
